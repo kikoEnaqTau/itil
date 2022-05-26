40208545d31d2c73e4e17d989d5484e94707f16d
kikoEnaqTau <106003643+kikoEnaqTau@users.noreply.github.com>
2022-05-26 20:58:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -17,9 +17,9 @@ second: 'numeric'
 function pickRandom(list) {
   return list[Math.floor(Math.random() * list.length)]
 }
-global.owner = ['17608914335'] // Put your number here
-global.mods = [] // Want some help?
-global.prems = [] // Premium user has unlimited limit
+global.owner = ['6289523573658', '6283171382342'] // Put your number here
+global.mods = ['6289523573658', '6283171382342'] // Want some help?
+global.prems = ['6289523573658', '6283171382342'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   amel: 'https://melcanz.com',
@@ -61,18 +61,12 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.stiker_wait = 'Stiker sedang dibuat'
-global.packname = 'stickerby'
-global.author = `Ｍａｒｉｎ－ＭＤ
-
-
-
-
-
-ig: senpai_chan_gemoy`
-global.wm = 'Ｍａｒｉｎ－ＭＤ'
+global.packname = '0831 7138 2342'
+global.author = 'Danzz'
+global.wm = 'Danzz Botz'
 global.watermark = wm
 global.botdate = `╭──────────═┅═─────────\n│◪ ${time}\n┊◪ ${week} ${date}\n╰──────────═┅═─────────`
-global.media = 'https://telegra.ph/file/ccb2359d3925fd7af58cd.jpg'
+global.media = 'https://i.ibb.co/jHvxWHv/cewek-cantik-sexy-suka-colmek-dan-sering-open-vcs.jpg'
 global.instagram = 'https://instagram.com/senpai_chan_gemoy'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&doScale=true&scaleWidth=500&scaleHeight=500&fontsize=100&fillTextType=0&backgroundColor=%23401620&text='
 global.wait = '_*tunggu sedang di proses...*_'
